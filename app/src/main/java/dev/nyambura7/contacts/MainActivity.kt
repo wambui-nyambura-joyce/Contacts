790bce8cb03e5ec1130d4922f2397ecacc2c5e5c
wambui-nyambura <nyamburawambuui@gmail.com>
2023-06-01 10:21:26 +0300

    first contac list

--- app/src/main/java/dev/nyambura7/contacts/MainActivity.kt
+++ app/src/main/java/dev/nyambura7/contacts/MainActivity.kt
@@ -32,8 +32,10 @@ class MainActivity : AppCompatActivity() {
         var contact11 = ContactData("","Esther","987654321","@esther")
         var contact12 = ContactData("","Parkire","0791234511","@parkire")
         var contact13 = ContactData("","Kevine","012234567898","@kevine")
+        var contact14 = ContactData("","Ian","012234567898","@ianmarwa")
+        var contact15 = ContactData("","Joan","0712345898","@joan")
 
-        var contactList = listOf<ContactData>(contact1,contact2,contact3,contact4,contact5,contact6,contact7,contact8,contact9,contact10,contact11,contact12,contact13)
+        var contactList = listOf<ContactData>(contact1,contact2,contact3,contact4,contact5,contact6,contact7,contact8,contact9,contact10,contact11,contact12,contact13,contact14,contact15)
         binding.rvContact.layoutManager = LinearLayoutManager(this)
         var ContactsRvAdapter = ContactsRvAdapter(contactList)
         binding.rvContact.adapter = ContactsRvAdapter
